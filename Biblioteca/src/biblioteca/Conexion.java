9f34c79d6ee5221c36da33819a224f4b18f5165e
Ricardo Pérez Palacios <ricardoperezpalacios@gmail.com>
2016-04-20 22:56:48 -0500

    add class
    Las clases que nos proporciono el maestro para la conexion con la base
    de datos

---
+++ Biblioteca/src/biblioteca/Conexion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Gustavo
+ */
+public class Conexion {
+    
+    public static final String URL = "jdbc:mysql://127.0.0.1/prueba";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "danae";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return connection;
+    }
+    
+}
