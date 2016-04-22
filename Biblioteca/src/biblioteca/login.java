2e876d17b3085b08e7cc25e2696865e908e9ae73
Saul879777 <saul879777@hotmail.com>
2016-04-22 10:29:39 -0500

    Corrección de título

--- Biblioteca/src/biblioteca/login.java
+++ Biblioteca/src/biblioteca/login.java
@@ -239,6 +239,14 @@ public class Login extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
